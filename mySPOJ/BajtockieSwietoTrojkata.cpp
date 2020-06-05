2982a57f7f3c487145a18fae660cba437baf2a99
Pawel Iskra <52103903+Pawel-Iskra@users.noreply.github.com>
2020-06-05 19:35:01 +0200

    Add files via upload

---
+++ mySPOJ/BajtockieSwietoTrojkata.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t, i, n, j, a, b, c;
+    long result;
+    double sum, k, x, area;
+
+    cin >> t;
+    for (i = 0; i < t; i++) {
+        sum = 0;
+        cin >> n;
+        cin >> k;
+        k = k / 10;
+
+        for (j = 0; j < n; j++) {
+            cin >> a >> b >> c;
+            if (abs(b - c) < a && a < (b + c)) {
+                area = sqrt((double)(a + b + c)
+                    * (double)(a + b - c)
+                    * (double)(a + c - b)
+                    * (double)(b + c - a)) / 4;
+                sum = sum + k * area;
+            }
+        }
+        result = round(sum);
+        cout << result << "\n";
+    }
+    return 0;
+}
