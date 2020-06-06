5fd0722b1cf7365db89c8b89be0534148dea50d3
Pawel Iskra <52103903+Pawel-Iskra@users.noreply.github.com>
2020-06-06 20:18:42 +0200

    Update NWW2.java

--- mySPOJ/NWW2.java
+++ mySPOJ/NWW2.java
@@ -40,13 +40,11 @@ public class NWW2 {
 
         private void fillBuffer() throws IOException {
             bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
-            if (bytesRead == -1)
-                buffer[0] = -1;
+            if (bytesRead == -1) buffer[0] = -1;
         }
 
         private byte read() throws IOException {
-            if (bufferPointer == bytesRead)
-                fillBuffer();
+            if (bufferPointer == bytesRead)fillBuffer();
             return buffer[bufferPointer++];
         }
     }
