95cb9638704198b67d7501d744b37845b73afbef
Pawel Iskra <52103903+Pawel-Iskra@users.noreply.github.com>
2020-06-10 11:08:46 +0200

    Add files via upload

---
+++ mySPOJ/GrayCode.cpp
@@ -0,0 +1,34 @@
+﻿///////////////////////////////////////////////////////
+/// NOTES: ////////////////////////////////////////////
+/// TASK ID = 655, NAME: Gray code ////////////////////
+/// link: https://pl.spoj.com/problems/PP0505D ////////
+/// TIME RESULT FOR THIS SOLUTION = 0,01s /////////////
+/// USAGE: GENERATING ALL POSIBBLE BINARY STRINGS /////
+/// OF A GIVEN LENGTH IN THE ORDER OF GRAY CODE ///////
+///////////////////////////////////////////////////////
+
+#include <iostream>
+#include <string>
+#include <math.h>
+#include <bitset>
+using namespace std;
+
+
+int main() {
+	int t, n, i, j, halfI, power, res;
+	string result;
+
+	cin >> t;
+	while (t-- > 0) {
+		cin >> n;
+		power = pow(2, n);
+		for (i = 0; i < power; i++) {
+			halfI = i / 2;
+			res = i ^ halfI;
+			result = bitset<32>(res).to_string();
+			for (j = 0; j < 32 - n; j++) result.erase(0, 1);
+			cout << result << "\n";
+		}
+	}
+	return 0;
+}
